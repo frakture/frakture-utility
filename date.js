dc0ff559ea28b75bd939aad1ae957dc349d3224b
Chris Lundberg <chris@frakture.com>
2015-09-25 02:36:09 +0000

    new alt date check

--- date.js
+++ date.js
@@ -48,18 +48,21 @@ exports.generateInclusiveArray=function(options){
 
 /*
 	
-	Return a Date object if it's a Date, or if it's an ISO 8601 date string
+	Return a Date object if it's a Date, or if it's an ISO 8601 date string, or if it matches 
 	
 */
 
 exports.dateRegex= /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+exports.dateRegexLegacy= /^[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2,4}$/;
 exports.checkDate=function(d){
 	if (!d) return d;
 	if (d instanceof Date) return d;
-	if (typeof d=='string' && d.match(exports.dateRegex)){
-		var newDate=new Date(d);
-		if (newDate=="Invalid Date") return d;
-		return newDate;
+	if (typeof d=='string'){
+		if (d.match(exports.dateRegex) || d.match(exports.dateRegexLegacy)){
+			var newDate=new Date(d);
+			if (newDate=="Invalid Date") return d;
+			return newDate;
+		}
 	}
 	return d;
 }
